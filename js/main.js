2b955e64a784140d53a58ba359073f769d5a47b1
Self-Z <alikealeksey@gmail.com>
2024-10-02 19:21:24 +0300

    new update (slick)

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ $(function () {
       $('.ourBlog__button-box-btn').removeClass('btn-active')
       $(this).addClass('btn-active')
    })
+
+   $('.coments__box').slick({
+      arrows: false,
+      slidesToShow: 2,
+      Infinity: true,
+   })
+   $('.coments__box-next').on('click', function (e) {
+      e.preventDefault()
+      $('.coments__box').slick('slickNext')
+   })
+   $('coments__box-prew').on('click', function (e) {
+      e.preventDefault()
+      $('.coments__box').slick('slickPrev')
+   })
 })
 
 
